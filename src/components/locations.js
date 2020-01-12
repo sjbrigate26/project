eabf580f88af98fe19f11bb972816ef5debd3c68
tarunshrm864 <tarunshrm864@gmail.com>
2020-01-12 23:14:27 +0530

    Fandom

---
+++ src/components/locations.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import '../css/locations.css';
+
+
+class Locations extends React.Component{
+    state={
+        loading: true,
+        planets : [],
+    };
+    async componentDidMount(){
+        const url="https://rickandmortyapi.com/api/location/";
+        const response=await fetch(url);
+        const data=await response.json();
+        this.setState({planets: data.results, loading:false})
+        console.log(data.results);
+    }
+    render(){
+        if (this.state.loading)
+        {
+            return <div>This Page is currently loading</div>;
+        }
+        if(!this.state.planets.length)
+        {
+            return <div>Didn't get a planet</div>;
+        }    
+        return(
+            <div>
+                <h1>Locations</h1>
+                {this.state.planets.map(planet =>(
+                    <div className="planet" key={planet.id} >
+                        
+                        <a href="./resident.js" onClick={()=> {sessionStorage.setItem("id",planet.id)}} style={{ textDecoration: 'none' }}>
+                        <div className="name" >{planet.name}</div>
+                        <div className="type">{planet.type}</div>
+                        <div className="resident">{planet.residents.length} Residents</div>
+                        </a>
+                    </div>
+                ) )}
+            </div>
+        );
+    }
+}
+export default Locations;
---
+++ src/components/sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../css/sidebar.css';
+
+
+function Sidebar(){
+    return(
+        <div>
+            <div className="sidebar">
+                <nav>
+                    <a className="link" href="/">Home</a>
+                    <a className="link" href="/locations">Locations</a>         
+                </nav>
+            </div>                
+        </div>
+
+    );
+}
+
+export default Sidebar;
